0c58fc710d9b3278e5a9098a5b03b36e66617be1
altarulas <altarulas09@gmail.com>
2024-04-07 09:59:02 +0300

    fix: fix build errors

--- actions/signup.ts
+++ actions/signup.ts
@@ -3,10 +3,10 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-import { createClient } from "@/utils/supabase/server";
+import { createSupabaseServerClient } from "@/utils/supabase/server";
 
 export async function signup(formData: FormData) {
-  const supabase = createClient();
+  const supabase = createSupabaseServerClient();
 
   // type-casting here for convenience
   // in practice, you should validate your inputs
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export default async function createSupabaseServerClient() {
+export function createSupabaseServerClient() {
   const cookieStore = cookies();
 
   // Create a server's supabase client with newly configured cookie,
