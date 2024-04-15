12564022ca5fa8a9d74ae0cbfe29cd5a90347f97
altarulas <altarulas09@gmail.com>
2024-04-15 14:58:06 +0300

    commit: add redux store and slices

--- components/core/Content.tsx
+++ components/core/Content.tsx
@@ -14,15 +14,28 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "../ui/textarea";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addEssayAndFeedback,
+  addQuestion,
+} from "@/store/features/tempEssaySlice";
+import { AppDispatch, RootState } from "@/store/store";
 
 export const Content = () => {
   const supabase = supabaseClient();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const essay_question = useSelector(
+    (state: RootState) => state.tempEssay.essay_question
+  );
+  const essay_feedback = useSelector(
+    (state: RootState) => state.tempEssay.essay_feedback
+  );
+
   const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+
   const [selectedTopic, setSelectedTopic] = useState("");
-  const [topic, setTopic] = useState<string>("");
   const [essay, setEssay] = useState("");
-  const [feedback, setFeedback] = useState("");
 
   const handleSelectChange = (value: string) => {
     setSelectedTopic(value);
@@ -32,50 +45,27 @@ export const Content = () => {
     setEssay(event.target.value);
   };
 
-  const createTopic = async () => {
+  const handleCreateTopic = async () => {
     setLoading(true);
-    setError(null);
-    try {
-      const { data, error } = await supabase.functions.invoke("openai", {
-        body: {
-          query: `Can you create me a ${selectedTopic} essay question topic. the topic question should be at least 150 char and maximum 250 char. the question should be formatted based on IELTS exams.`,
-        },
-      });
-      if (error) {
-        throw new Error(error.message);
-      }
-      setTopic(data);
-    } catch (error) {
-      console.error("Error calling edge function:", error);
-      setError("An error occurred while calling the edge function.");
-    } finally {
-      setLoading(false);
-    }
+    await dispatch(addQuestion(selectedTopic));
+    setLoading(false);
   };
 
-  const giveFeedback = async () => {
+  const handleFinishEssay = async () => {
+    alert("nice job essay is finished...");
+  };
+
+  const handleGetFeedback = async () => {
     setLoading(true);
-    setError(null);
-    try {
-      const { data, error } = await supabase.functions.invoke("openai", {
-        body: {
-          query: `Calculate the mark of given essay from scale 1 to 9. The calculation of mark should be based on IELTS exams. After calculation, give feedbacks to improve the essay and make some suggestions. This is the question topic of essay: ${topic}. This is the essay given by the user: ${essay}`,
-        },
-      });
-      if (error) {
-        throw new Error(error.message);
-      }
-      setFeedback(data);
-    } catch (error) {
-      console.error("Error calling edge function:", error);
-      setError("An error occurred while calling the edge function.");
-    } finally {
-      setLoading(false);
-    }
+    await dispatch(
+      addEssayAndFeedback({ essay_text: essay, essay_question: essay_question })
+    );
+    setLoading(false);
   };
 
   return (
     <div className="flex flex-col gap-10 items-center w-full h-full py-10 px-10">
+      {essay_question && "time started..."}
       <div className="w-full gap-10 flex justify-center">
         <Select onValueChange={(value) => handleSelectChange(value)}>
           <SelectTrigger className="w-[180px]">
@@ -93,17 +83,21 @@ export const Content = () => {
           </SelectContent>
         </Select>
 
-        <Button onClick={createTopic} disabled={loading}>
+        <Button onClick={handleCreateTopic} disabled={loading}>
           Create Topic
         </Button>
 
-        <Button onClick={giveFeedback} disabled={loading}>
+        <Button onClick={handleFinishEssay} disabled={loading}>
+          Finish Essay
+        </Button>
+
+        <Button onClick={handleGetFeedback} disabled={loading}>
           Give Feedback
         </Button>
       </div>
 
       <Card className="w-full h-20">
-        {loading ? "Loading..." : error ? error : topic}
+        {loading ? "Loading..." : essay_question}
       </Card>
 
       <Textarea
@@ -113,7 +107,7 @@ export const Content = () => {
       />
 
       <Card className="w-full h-96">
-        {loading ? "Loading..." : error ? error : feedback}
+        {loading ? "Loading..." : essay_feedback}
       </Card>
     </div>
   );
---
+++ store/features/userInfoSlice.ts
@@ -0,0 +1,57 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { supabaseClient } from "@/utils/supabase/client";
+
+const supabase = supabaseClient();
+
+interface IUserInfo {
+  email_address: string;
+  full_name: string;
+  avatar_url: string;
+}
+
+const initialState: IUserInfo = {
+  email_address: "",
+  full_name: "",
+  avatar_url: "",
+};
+
+export const getUserInfo = createAsyncThunk(
+  "userInfoSlice/getUserInfo",
+  async () => {
+    const id = (await supabase.auth.getUser()).data.user?.aud;
+
+    try {
+      const { data, error } = await supabase
+        .from("profiles")
+        .select("email_address, full_name, avatar_url")
+        .eq("id", id)
+        .single();
+
+      if (!error) {
+        const user: IUserInfo = data;
+        return user;
+      } else {
+        throw new Error(error.message);
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+);
+
+export const UserInfoSlice = createSlice({
+  name: "userInfo",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getUserInfo.fulfilled, (state, action) => {
+      const { email_address, full_name, avatar_url } = action.payload;
+      state.email_address = email_address;
+      state.full_name = full_name;
+      state.avatar_url = avatar_url;
+    });
+  },
+});
+
+export default UserInfoSlice.reducer;
